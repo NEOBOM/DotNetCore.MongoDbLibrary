b6b572cb83008bb8e831e2a4286985a773116c6c
Everson M. de Campos <baddexter@hotmail.com>
2019-11-17 17:31:46 -0300

    Alter struture

---
+++ src/MongoDbLibrary.Mongo/MongoDbClient.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDbLibrary.Mongo.Properties;
+using System;
+
+namespace MongoDbLibrary.Mongo
+{
+    public sealed class MongoDbClient
+    {
+        private readonly IMongoDatabase _db = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectionString">Connection string</param>
+        /// <param name="baseName">Base name access</param>
+        public MongoDbClient(string connectionString, string baseName) : this(new MongoClient(connectionString), baseName)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mongoClient">MongoClient instance.</param>
+        /// <param name="mongoDbName">DataBase name access</param>
+        public MongoDbClient(IMongoClient mongoClient, string mongoDbName)
+        {
+            if (mongoClient == null)
+                throw new Exception(Resources.MESSAGE_MONGO_CLIENT_INVALID);
+
+            if (string.IsNullOrEmpty(mongoDbName))
+                throw new Exception(Resources.MESSAGE_MONGO_DB_NAME_INVALID);
+
+            _db = mongoClient.GetDatabase(mongoDbName);
+
+            var pack = new ConventionPack();
+            pack.Add(new IgnoreExtraElementsConvention(true));
+            ConventionRegistry.Register("onventions", pack, t => true);
+        }
+        
+
+        /// <summary>
+        /// Get collection by name
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="collectionName">Collection name access</param>
+        /// <returns></returns>
+        public IMongoCollection<TEntity> Collection<TEntity>(string collectionName) where TEntity : class => _db.GetCollection<TEntity>(collectionName);
+    }
+}
